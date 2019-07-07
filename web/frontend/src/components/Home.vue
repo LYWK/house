49f6a684d7c9e6b88866af4dc3457e7d1cfa1681
lyeman <lyeman@naver.com>
2019-07-08 00:44:40 +0900

    0708

---
+++ web/frontend/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+     <Nav></Nav>
+     <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/common/Nav.vue'
+import Footer from'@/components/common/Footer.vue'
+import JumboTron from'@/components/common/JumboTron.vue'
+
+export default {
+  name: 'Home',
+  components:{
+      Nav,Footer,JumboTron
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ web/frontend/src/components/member/LoginForm.vue
@@ -0,0 +1,70 @@
+<template>
+<div>
+  <Nav></Nav>
+  <form>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox"> Remember me</label>
+    </div>
+    <button class="btn btn-default" @click="get">조 회</button>
+    <button class="btn btn-default" @click="post">입 력</button>
+    <button class="btn btn-default" @click="put">수 정</button>
+    <button class="btn btn-default" @click="del">삭 제</button>
+    
+    
+  </form>
+  <Footer></Footer>
+</div>
+</template>
+
+<script>
+import Nav from '@/components/common/Nav.vue'
+import Footer from'@/components/common/Footer.vue'
+import axios from 'axios'
+export default {
+  components: {
+    Nav,
+    Footer
+  },
+  methods:{
+      get: ()=>{
+         axios.get('/customers/hong')
+         .then(d=>{
+           
+             alert(`SUCCESS : ${d.data.customerId}`)
+         })
+         .catch(e=>{
+             alert('ERROR')
+         })
+      },
+      post: ()=>{
+        axios.post('/customers')
+        .then(d=>{
+          alert(`POST 연동성공: ${d.data.result}`)
+        })
+      },
+      put: ()=>{
+        axios.put('/customers/id')
+        .then(d=>{
+          alert(`PUT 연동성공: ${d.data.result}`)
+        })
+      },
+      del: ()=>{
+        axios.delete('/customers/id')
+        .then(d=>{
+          alert(`DEL 연동성공: ${d.data.result}`)
+        })
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
